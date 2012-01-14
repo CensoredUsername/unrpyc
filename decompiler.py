9b4c2798a9d8eb8c006396e1b05df21d4fb6b6dc
yuriks <yuriks@yuriks.net>
2012-01-14 01:56:21 -0200

    Added basic decompilation support.
    Supports only labels and text right now, but at least it does something.
    Basic infrastructure is in place, so now all that needs to be done is add
    support for more node types.
    
    Also need to figure out what's up with the children-less label nodes and if
    I need to indent stuff.

--- decompiler.py
+++ decompiler.py
@@ -21,4 +21,42 @@
 import renpy.ast as ast
 
 def pretty_print_ast(out_file, ast):
-    pass
+    for stmt in ast:
+        print_statement(out_file, stmt, 0)
+
+def print_statement(f, statement, indent_level=0):
+    # Print indentation
+    f.write(u'\t' * indent_level)
+
+    statement_printer_dict.get(statement.__class__, print_Unknown)(f, statement)
+
+def escape_string(s):
+    s = s.replace('"', '\\"')
+    s = s.replace('\n', '\\n')
+    s = s.replace('\t', '\\t')
+    return s
+
+def print_Label(f, stmt):
+    f.write(u"label %s" % (stmt.name, ))
+    # TODO parameters
+    f.write(u':\n')
+
+def print_Say(f, stmt):
+    if stmt.who is not None:
+        f.write(u"%s " % (stmt.who, ))
+        # TODO who_fast
+    f.write(u"\"%s\"" % (escape_string(stmt.what), ))
+    if stmt.with_ is not None:
+        f.write(u" with TODO")
+        # TODO with_
+    # TODO interact
+    f.write(u'\n')
+
+statement_printer_dict = {
+        ast.Label: print_Label,
+        ast.Say: print_Say
+    }
+
+def print_Unknown(f, stmt):
+    print "Unknown AST node: %s" % (stmt.__class__.__name__, )
+    f.write(u"<<<UNKNOWN NODE %s>>>\n" % (stmt.__class__.__name__, ))
