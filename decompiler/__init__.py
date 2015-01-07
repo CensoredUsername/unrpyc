050939351dc6e99dc8aa276e015e2b0abd046335
Jackmcbarn <jackmcbarn@users.noreply.github.com>
2015-01-07 18:56:57 -0500

    Handle attributes on say statements

--- decompiler/__init__.py
+++ decompiler/__init__.py
@@ -541,6 +541,9 @@ class Decompiler(DecompilerBase):
         self.indent()
         if ast.who is not None:
             self.write("%s " % ast.who)
+        if ast.attributes is not None:
+            for i in ast.attributes:
+                self.write("%s " % i)
         self.write('"%s"' % string_escape(ast.what))
         if not ast.interact and not inmenu:
             self.write(" nointeract")
