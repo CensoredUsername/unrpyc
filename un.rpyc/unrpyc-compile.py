40c26003c7024585a3f3388820dbe8afd4d2f8d2
Jackmcbarn <jackmcbarn@users.noreply.github.com>
2015-01-10 17:58:27 -0500

    Remove force_multiline_kwargs from unrpyc-compile

--- un.rpyc/unrpyc-compile.py
+++ un.rpyc/unrpyc-compile.py
@@ -61,8 +61,7 @@ def decompile_rpyc(file_obj, abspath):
 
     ensure_dir(out_filename)
     with codecs.open(out_filename, 'w', encoding='utf-8') as out_file:
-        decompiler.pprint(out_file, ast, force_multiline_kwargs=True,
-                                             decompile_screencode=True,
+        decompiler.pprint(out_file, ast, decompile_screencode=True,
                                              decompile_python=True,
                                              comparable=False)
     return True
