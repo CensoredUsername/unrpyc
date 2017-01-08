4a880eb3e22f1b6966a16a065200fc427d6b1b9b
Jackmcbarn <jackmcbarn@users.noreply.github.com>
2017-01-08 15:34:27 -0500

    Fix build

--- make-bintray-json.sh
+++ make-bintray-json.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sed -e s/BRANCHNAME/$(git symbolic-ref --short HEAD)/ -e s/GITREV/$(git rev-parse --short HEAD)/ bintray-template.json > bintray.json
+sed -e s/BRANCHNAME/${TRAVIS_BRANCH}/ -e s/GITREV/$(git rev-parse --short HEAD)/ bintray-template.json > bintray.json
