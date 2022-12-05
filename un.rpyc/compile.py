75f2021ed903e7778a11be5dbad0faa9cc15c7a2
madeddy <mad-eddy@gmx.de>
2022-12-05 18:07:03 +0100

    correct shebang

--- un.rpyc/compile.py
+++ un.rpyc/compile.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/python3
 
 # Copyright (c) 2014 CensoredUsername
 #
--- unrpyc.py
+++ unrpyc.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/python3
 
 # Copyright (c) 2012 Yuri K. Schlesner
 #
